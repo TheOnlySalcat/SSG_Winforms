03adc17c35c7932717eacf914ca987e1051a8119
TheOnlySalcat <74712815+TheOnlySalcat@users.noreply.github.com>
2020-11-19 14:21:35 +0000

    MVP
    Technically meets the requirements, but leaves much to be desired

--- SSG_Winforms/SSG_Winforms/Form1.Designer.cs
+++ SSG_Winforms/SSG_Winforms/Form1.Designer.cs
@@ -28,45 +28,104 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.btnTest = new System.Windows.Forms.Button();
-            this.txtTest = new System.Windows.Forms.TextBox();
+            this.txtBookName = new System.Windows.Forms.TextBox();
+            this.lblBookName = new System.Windows.Forms.Label();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.lbBooksResult = new System.Windows.Forms.ListBox();
+            this.lblBook = new System.Windows.Forms.Label();
+            this.lblChapter = new System.Windows.Forms.Label();
+            this.lbChaptersResult = new System.Windows.Forms.ListBox();
             this.SuspendLayout();
             // 
-            // btnTest
+            // txtBookName
             // 
-            this.btnTest.Location = new System.Drawing.Point(12, 86);
-            this.btnTest.Name = "btnTest";
-            this.btnTest.Size = new System.Drawing.Size(75, 23);
-            this.btnTest.TabIndex = 0;
-            this.btnTest.Text = "Test";
-            this.btnTest.UseVisualStyleBackColor = true;
-            this.btnTest.Click += new System.EventHandler(this.btnTest_Click);
+            this.txtBookName.Location = new System.Drawing.Point(93, 15);
+            this.txtBookName.Name = "txtBookName";
+            this.txtBookName.Size = new System.Drawing.Size(311, 20);
+            this.txtBookName.TabIndex = 1;
+            this.txtBookName.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtBookName_KeyDown);
             // 
-            // txtTest
+            // lblBookName
             // 
-            this.txtTest.Location = new System.Drawing.Point(12, 115);
-            this.txtTest.Name = "txtTest";
-            this.txtTest.Size = new System.Drawing.Size(709, 20);
-            this.txtTest.TabIndex = 1;
+            this.lblBookName.AutoSize = true;
+            this.lblBookName.Location = new System.Drawing.Point(9, 18);
+            this.lblBookName.Name = "lblBookName";
+            this.lblBookName.Size = new System.Drawing.Size(63, 13);
+            this.lblBookName.TabIndex = 2;
+            this.lblBookName.Text = "Book Name";
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(329, 41);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(75, 23);
+            this.btnSearch.TabIndex = 3;
+            this.btnSearch.Text = "Search";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // lbBooksResult
+            // 
+            this.lbBooksResult.FormattingEnabled = true;
+            this.lbBooksResult.Location = new System.Drawing.Point(12, 114);
+            this.lbBooksResult.Name = "lbBooksResult";
+            this.lbBooksResult.Size = new System.Drawing.Size(392, 121);
+            this.lbBooksResult.TabIndex = 4;
+            this.lbBooksResult.SelectedIndexChanged += new System.EventHandler(this.lbBooksResult_SelectedIndexChanged);
+            // 
+            // lblBook
+            // 
+            this.lblBook.AutoSize = true;
+            this.lblBook.Location = new System.Drawing.Point(12, 95);
+            this.lblBook.Name = "lblBook";
+            this.lblBook.Size = new System.Drawing.Size(65, 13);
+            this.lblBook.TabIndex = 5;
+            this.lblBook.Text = "Select Book";
+            // 
+            // lblChapter
+            // 
+            this.lblChapter.AutoSize = true;
+            this.lblChapter.Location = new System.Drawing.Point(12, 259);
+            this.lblChapter.Name = "lblChapter";
+            this.lblChapter.Size = new System.Drawing.Size(77, 13);
+            this.lblChapter.TabIndex = 6;
+            this.lblChapter.Text = "Select Chapter";
+            // 
+            // lbChaptersResult
+            // 
+            this.lbChaptersResult.FormattingEnabled = true;
+            this.lbChaptersResult.Location = new System.Drawing.Point(12, 276);
+            this.lbChaptersResult.Name = "lbChaptersResult";
+            this.lbChaptersResult.Size = new System.Drawing.Size(392, 121);
+            this.lbChaptersResult.TabIndex = 7;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.txtTest);
-            this.Controls.Add(this.btnTest);
+            this.ClientSize = new System.Drawing.Size(416, 416);
+            this.Controls.Add(this.lbChaptersResult);
+            this.Controls.Add(this.lblChapter);
+            this.Controls.Add(this.lblBook);
+            this.Controls.Add(this.lbBooksResult);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.lblBookName);
+            this.Controls.Add(this.txtBookName);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Bindle - Like Kindle except it\'s trash";
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Button btnTest;
-        private System.Windows.Forms.TextBox txtTest;
+        private System.Windows.Forms.TextBox txtBookName;
+        private System.Windows.Forms.Label lblBookName;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.ListBox lbBooksResult;
+        private System.Windows.Forms.Label lblBook;
+        private System.Windows.Forms.Label lblChapter;
+        private System.Windows.Forms.ListBox lbChaptersResult;
     }
 }
 
--- SSG_Winforms/SSG_Winforms/Form1.cs
+++ SSG_Winforms/SSG_Winforms/Form1.cs
@@ -11,34 +11,83 @@ using System.Net.Http;
 using System.Net.Http.Headers;
 using Newtonsoft.Json;
 using RestSharp;
+using System.Web.UI.WebControls;
 
 namespace SSG_Winforms
 {
     public partial class Form1 : Form
     {
+        //Settings which may be re-used
+        public string apiBaseUrl = "https://the-one-api.dev/v2";
+        private string apiToken = "x4IHDwrEg5b11yXtRQxj";
+        
         public Form1()
         {
             InitializeComponent();
+            
         }
 
-        private void btnTest_Click(object sender, EventArgs e)
+        private void btnSearch_Click(object sender, EventArgs e)
         {
-            // API token x4IHDwrEg5b11yXtRQxj
-            var client = new RestClient("https://the-one-api.dev/v2/book");
+            //Call API using CONTAINS operator (generally most user friendly)
+            var client = new RestClient($"{apiBaseUrl}/book?name=/{txtBookName.Text}/i");
             client.Timeout = -1;
             var request = new RestRequest(Method.GET);
-            request.AddHeader("Authorization", "Bearer x4IHDwrEg5b11yXtRQxj");
+            request.AddHeader("Authorization", $"Bearer {apiToken}");
             IRestResponse response = client.Execute(request);
-            txtTest.Text = response.Content;
 
             books booksList = JsonConvert.DeserializeObject<books>(response.Content);
 
-            MessageBox.Show($"found {booksList.docs.Count} books");
+            //Put results into listbox
+            lbBooksResult.Items.Clear();
+
+            foreach (book b in booksList.docs)
+            {
+                ListItem item = new ListItem();
+                item.Text = b.name;
+                item.Value = b._id;
+                lbBooksResult.Items.Add(item);
+            }
         }
+
+    
+        #region Events
+        private void txtBookName_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+                btnSearch_Click(sender, e);
+        }
+
+        private void lbBooksResult_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (lbBooksResult.SelectedItem != null)
+            {
+                //Call API - Get chapters for book
+                var client = new RestClient($"{apiBaseUrl}/chapter?book={lbBooksResult.SelectedValue}");
+                client.Timeout = -1;
+                var request = new RestRequest(Method.GET);
+                request.AddHeader("Authorization", $"Bearer {apiToken}");
+                IRestResponse response = client.Execute(request);
+
+                chapters chaptersList = JsonConvert.DeserializeObject<chapters>(response.Content);
+
+                //Put results into listbox
+                lbChaptersResult.Items.Clear();
+
+                foreach (chapter c in chaptersList.docs)
+                {
+                    ListItem item = new ListItem();
+                    item.Text = c.chapterName;
+                    item.Value = c._id;
+                    lbChaptersResult.Items.Add(item);
+                }
+            }
+        }
+        #endregion
     }
 
     #region ObjectDefinitions
-    
+
     public class book
     {
         public string _id { get; set; }
@@ -48,6 +97,23 @@ namespace SSG_Winforms
     public class books
     {
         public List<book> docs { get; set; }
+        public int total { get; set; }
+        public int limit { get; set; }
+        public int offset { get; set; }
+        public int page { get; set; }
+        public int pages { get; set; }
+    }
+
+    public class chapter
+    {
+        public string _id { get; set; }
+        public string chapterName { get; set; }
+        public string book { get; set; }
+    }
+
+    public class chapters
+    {
+        public List<chapter> docs { get; set; }
         public int total { get; set; }
         public int limit { get; set; }
         public int offset { get; set; }
